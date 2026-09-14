e2ffb0931288b39d659ff03b780a50233629e6d4
Azhar Khan <azhar.khan.78601@gmail.com>
2026-09-14 22:15:23 +0530

    feat: add async spawn, zero-dependency CLI, and WSL support (#25)
    * feat: add async spawn, zero-dependency CLI, and WSL support
    
    * ci: run tests in pre-commit hook

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,14 @@
-declare function open(url: string): Promise<void>;
+import type { ChildProcess } from 'child_process';
+
+export interface OpenOptions {
+  /**
+   * Wait for the browser/app process to terminate.
+   * @default false
+   */
+  wait?: boolean;
+}
+
+declare function open(url: string, options?: OpenOptions): Promise<ChildProcess>;
 
 export { open };
+export default open;
